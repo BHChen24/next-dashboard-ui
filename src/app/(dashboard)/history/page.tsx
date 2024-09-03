d9656b12db195ee326d118660c360da693105ad6
Baihua Chen <bchen102@myseneca.ca>
2024-09-02 23:52:13 -0400

    Table list in progress

--- src/app/(dashboard)/history/page.tsx
+++ src/app/(dashboard)/history/page.tsx
@@ -1,5 +1,32 @@
+import ListSearch from "@/components/ListSearch";
+import Pagination from "@/components/Pagination";
+import Image from "next/image";
+
 export default function HistoryPage() {
-    return (
-        <div> test </div>
-    );
+  return (
+    <div className="bg-white rounded-md p-4 flex-1 m-4 mt-0">
+      {/* top */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-lg font-semibold md:block hidden">History</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <ListSearch />
+          <div className="flex self-end items-center gap-4">
+            <div className="w-8 h-8 flex items-center justify-center rounded-full">
+              <Image src="/filter.png" alt="" width={14} height={14}></Image>
+            </div>
+            <div className="w-8 h-8 flex items-center justify-center rounded-full">
+              <Image src="/sort.png" alt="" width={14} height={14}></Image>
+            </div>
+            <div className="w-8 h-8 flex items-center justify-center rounded-full">
+              <Image src="/plus.png" alt="" width={14} height={14}></Image>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* list */}
+      <div className=""></div>
+      {/* Pagination */}
+      <Pagination />
+    </div>
+  );
 }
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,15 @@
+export default function Pagination() {
+    return (
+        <div className="p-4 flex items-center justify-between text-gray-500">
+            <div className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:cursor-not-allowed disabled:opacity-50">Prev</div>
+            <div className="flex gap-2 items-center text-sm">
+                <button className="rounded-sm px-2 bg-blue-200">1</button>
+                <button className="rounded-sm px-2">2</button>
+                <button className="rounded-sm px-2">3</button>
+                ...
+                <button className="rounded-sm px-2">10</button>
+            </div>
+            <div className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:cursor-not-allowed disabled:opacity-50">Next</div>
+        </div>
+    );
+}
